26c9d656233411612aa1b0724a57ea079863403c
Daniël de Kok <me@danieldk.eu>
2019-09-23 10:20:16 +0200

    de-deps-ud-small: init at 20190923

--- pkgs/models/default.nix
+++ pkgs/models/default.nix
@@ -13,11 +13,26 @@ let
     filename = "${name}.fifu";
   };
 
+  deUdSttsTagEmbeds = fetchEmbeddings {
+    name = "de-structgram-tags-ud-20190629";
+    sha256 = "1dglh4y04v9nagxa7rjlh4381652gmv53v64asin09x69wa3hi2j";
+  };
   deWordEmbeds = fetchEmbeddings {
     name = "de-structgram-20190426-opq";
     sha256 = "0b75bpsrfxh173ssa91pql3xmvd7x9f2qwc6rv27jj6pxlhayfql";
   };
 in lib.mapAttrs (_: value: recurseIntoAttrs value) {
+  de-deps-ud-small = stickerModel {
+    inherit stdenvNoCC fetchurl makeWrapper sticker;
+
+    modelName = "de-deps-ud-small";
+    version = "20190923";
+    sha256 = "1q405xmdlvwndf8y8pij89qjkd5i43x35vwfpijzq729s7ry3mpc";
+
+    wordEmbeds = deWordEmbeds;
+    tagEmbeds = deUdSttsTagEmbeds;
+  };
+
   de-pos-ud = stickerModel {
     inherit stdenvNoCC fetchurl makeWrapper sticker;
 
