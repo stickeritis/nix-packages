d907e76ff029847d28d1073dfc3c5bb6fc42f5e9
Daniël de Kok <me@danieldk.eu>
2020-01-29 09:43:40 +0100

    sticker2_models.nl-ud-medium: init at 20200129

--- pkgs/sticker2_models/default.nix
+++ pkgs/sticker2_models/default.nix
@@ -10,4 +10,12 @@ in lib.mapAttrs (_: value: recurseIntoAttrs value) {
     version = "20200128";
     sha256 = "0l8s2xy397ps1g4kqppcq57ahqnkc9bszm9wn8y3gisrblzl6fap";
   };
+
+  nl-ud-medium = stickerModel {
+    inherit lib stdenvNoCC fetchurl dockerTools makeWrapper sticker2;
+
+    modelName = "nl-ud-medium";
+    version = "20200129";
+    sha256 = "08ny3i2aspabnmnj9wgmpyarkad08ppm907rzz7j578w1ij8zwhp";
+  };
 }
