f174c9dad9b11b9cf9bf0abf19239993e43eeaf6
Daniël de Kok <me@danieldk.eu>
2020-02-01 10:44:33 +0100

    sticker2_models.nl-ud-small: init at 20200201

--- pkgs/sticker2_models/default.nix
+++ pkgs/sticker2_models/default.nix
@@ -18,4 +18,12 @@ in lib.mapAttrs (_: value: recurseIntoAttrs value) {
     version = "20200129";
     sha256 = "08ny3i2aspabnmnj9wgmpyarkad08ppm907rzz7j578w1ij8zwhp";
   };
+
+  nl-ud-small = stickerModel {
+    inherit lib stdenvNoCC fetchurl dockerTools makeWrapper sticker2;
+
+    modelName = "nl-ud-small";
+    version = "20200201";
+    sha256 = "032mjp6k1dwks21x16kdzpcsm5vh97py2ffai3dnwfr047a5p934";
+  };
 }
