cbbc6c852d4c8419d0217987e9e74e15ae76974b
Daniël de Kok <me@danieldk.eu>
2020-02-24 14:08:08 +0100

    sticker2_models: split up stickerModel arguments in two sets
    The first set specifies the dependencies and the second set the
    model-specific arguments. With this change we do not have to specify
    the dependencies for each stickerModel call.

--- pkgs/sticker2_models/default.nix
+++ pkgs/sticker2_models/default.nix
@@ -1,35 +1,29 @@
 { lib, recurseIntoAttrs, stdenvNoCC, fetchurl, dockerTools, makeWrapper, sticker2 }:
 
 let
-  stickerModel = import ./model.nix;
+  stickerModel = import ./model.nix {
+    inherit lib stdenvNoCC fetchurl dockerTools makeWrapper sticker2;
+  };
 in lib.mapAttrs (_: value: recurseIntoAttrs value) {
   de-ud-large = stickerModel {
-    inherit lib stdenvNoCC fetchurl dockerTools makeWrapper sticker2;
-
     modelName = "de-ud-large";
     version = "20200222";
     sha256 = "1czlnsxvanf69y6sxawi46q1fxs3xjf6ggvnkfr7zlgfs1i38hid";
   };
 
   nl-ud-large = stickerModel {
-    inherit lib stdenvNoCC fetchurl dockerTools makeWrapper sticker2;
-
     modelName = "nl-ud-large";
     version = "20200220";
     sha256 = "0ahqwkhc161i8vzbfxq2lvrdzv2rmp44gjyfg5vnpwjw00i0xk8q";
   };
 
   nl-ud-medium = stickerModel {
-    inherit lib stdenvNoCC fetchurl dockerTools makeWrapper sticker2;
-
     modelName = "nl-ud-medium";
     version = "20200129";
     sha256 = "08ny3i2aspabnmnj9wgmpyarkad08ppm907rzz7j578w1ij8zwhp";
   };
 
   nl-ud-small = stickerModel {
-    inherit lib stdenvNoCC fetchurl dockerTools makeWrapper sticker2;
-
     modelName = "nl-ud-small";
     version = "20200201";
     sha256 = "032mjp6k1dwks21x16kdzpcsm5vh97py2ffai3dnwfr047a5p934";
--- pkgs/sticker_models/default.nix
+++ pkgs/sticker_models/default.nix
@@ -1,8 +1,9 @@
 { lib, recurseIntoAttrs, stdenvNoCC, fetchurl, dockerTools, makeWrapper, sticker }:
 
 let
-  stickerModel = import ./model.nix;
-
+  stickerModel = import ./model.nix {
+    inherit lib stdenvNoCC fetchurl dockerTools makeWrapper sticker;
+  };
   # Embeddings fetcher, uses the sticker-models repository.
   fetchEmbeddings = { name, sha256 }: {
     embeds = fetchurl {
@@ -31,8 +32,6 @@ let
   };
 in lib.mapAttrs (_: value: recurseIntoAttrs value) {
   de-deps-ud-large = stickerModel {
-    inherit lib stdenvNoCC fetchurl dockerTools makeWrapper sticker;
-
     modelName = "de-deps-ud-large";
     version = "20190926";
     sha256 = "15jg0vkpd55833qbykzg1pp7kh34sm59vrvg07rz3cjg9cks8nw2";
@@ -42,8 +41,6 @@ in lib.mapAttrs (_: value: recurseIntoAttrs value) {
   };
 
   de-deps-ud-small = stickerModel {
-    inherit lib stdenvNoCC fetchurl dockerTools makeWrapper sticker;
-
     modelName = "de-deps-ud-small";
     version = "20190923";
     sha256 = "1q405xmdlvwndf8y8pij89qjkd5i43x35vwfpijzq729s7ry3mpc";
@@ -53,8 +50,6 @@ in lib.mapAttrs (_: value: recurseIntoAttrs value) {
   };
 
   de-ner-ud-small = stickerModel {
-    inherit lib stdenvNoCC fetchurl dockerTools makeWrapper sticker;
-
     modelName = "de-ner-ud-small";
     version = "20190928";
     sha256 = "01bay1bxhap9mmwmjgfr17z1w8f05h3d16czf9vl3b3r3a2b0q23";
@@ -64,8 +59,6 @@ in lib.mapAttrs (_: value: recurseIntoAttrs value) {
   };
 
   de-pos-ud = stickerModel {
-    inherit lib stdenvNoCC fetchurl dockerTools makeWrapper sticker;
-
     modelName = "de-pos-ud";
     version = "20190821";
     sha256 = "163himdn8l1ds6znf1girx0hbplbc8i9z9lmqdwvxsj9d9338svv";
@@ -74,8 +67,6 @@ in lib.mapAttrs (_: value: recurseIntoAttrs value) {
   };
 
   de-topo-ud-small = stickerModel {
-    inherit lib stdenvNoCC fetchurl dockerTools makeWrapper sticker;
-
     modelName = "de-topo-ud-small";
     version = "20191002";
     sha256 = "0d1xbyd2pfl29yz4rix952xnbc09nvhyjrfnmjzzqhx1dp253k96";
@@ -86,8 +77,6 @@ in lib.mapAttrs (_: value: recurseIntoAttrs value) {
 
 
   nl-deps-ud-large = stickerModel {
-    inherit lib stdenvNoCC fetchurl dockerTools makeWrapper sticker;
-
     modelName = "nl-deps-ud-large";
     version = "20190929";
     sha256 = "15rbmxhkb5pns3djw27dlcqxwgadhdgzadl6ph3n30pgq53x8m3b";
@@ -97,8 +86,6 @@ in lib.mapAttrs (_: value: recurseIntoAttrs value) {
   };
 
   nl-deps-ud-small = stickerModel {
-    inherit lib stdenvNoCC fetchurl dockerTools makeWrapper sticker;
-
     modelName = "nl-deps-ud-small";
     version = "20191025";
     sha256 = "04a981rxkpj8a95f4w8n3p7pqpys2d4ff03qj19j0gwvnznlqqnj";
@@ -108,8 +95,6 @@ in lib.mapAttrs (_: value: recurseIntoAttrs value) {
   };
 
   nl-ner-ud-small = stickerModel {
-    inherit lib stdenvNoCC fetchurl dockerTools makeWrapper sticker;
-
     modelName = "nl-ner-ud-small";
     version = "20191003";
     sha256 = "1mbbc7m2r3p6zvlc7sszr3jqjmgimn4x0as39j0qllqsa540hy9c";
@@ -120,8 +105,6 @@ in lib.mapAttrs (_: value: recurseIntoAttrs value) {
 
 
   nl-pos-ud = stickerModel {
-    inherit lib stdenvNoCC fetchurl dockerTools makeWrapper sticker;
-
     modelName = "nl-pos-ud";
     version = "20190822";
     sha256 = "0ywa0kmpsh1cmdcl4ya0q67wcjq4m6g2n79a1kjgrqmhydc7d59p";
