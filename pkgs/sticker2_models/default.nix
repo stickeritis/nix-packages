dde7772b36ce49e31be40a172df5120961d1e0b8
Daniël de Kok <me@danieldk.eu>
2020-03-04 13:32:19 +0100

    Purge older models that are not compatible with sticker2 0.2

--- pkgs/sticker2_models/default.nix
+++ pkgs/sticker2_models/default.nix
@@ -22,16 +22,4 @@ in lib.mapAttrs (_: value: recurseIntoAttrs value) {
     version = "20200220";
     sha256 = "0ahqwkhc161i8vzbfxq2lvrdzv2rmp44gjyfg5vnpwjw00i0xk8q";
   };
-
-  nl-ud-medium = stickerModel {
-    modelName = "nl-ud-medium";
-    version = "20200129";
-    sha256 = "08ny3i2aspabnmnj9wgmpyarkad08ppm907rzz7j578w1ij8zwhp";
-  };
-
-  nl-ud-small = stickerModel {
-    modelName = "nl-ud-small";
-    version = "20200201";
-    sha256 = "032mjp6k1dwks21x16kdzpcsm5vh97py2ffai3dnwfr047a5p934";
-  };
 }
