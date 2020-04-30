ac38fca72b789f7a85cdc590138bb5ea035358b5
Daniël de Kok <me@danieldk.eu>
2020-04-30 15:44:25 +0200

    sticker2_models.nl-ud-medium: init at 20200430

--- pkgs/sticker2_models/default.nix
+++ pkgs/sticker2_models/default.nix
@@ -10,4 +10,10 @@ in lib.mapAttrs (_: value: recurseIntoAttrs value) {
     version = "20200420";
     sha256 = "0gyajncfw9x0lsfvplkhfj72l0q47clrsp0x55gp368l2bqwj9z7";
   };
+
+  nl-ud-medium = stickerModel {
+    modelName = "nl-ud-medium";
+    version = "20200430";
+    sha256 = "0z62smmkc43yiw8fbnfqvlmk7f70qgyjrh41mj6q9y4p4bpa2igz";
+  };
 }
