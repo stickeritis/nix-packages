12241d664f9642fc1b534315fc899130c4b4fb59
Daniël de Kok <me@danieldk.eu>
2020-07-10 11:19:58 +0200

    libtorch: 1.5.0 -> 1.5.1

--- pkgs/libtorch/default.nix
+++ pkgs/libtorch/default.nix
@@ -3,19 +3,19 @@
 callPackage ./generic.nix rec {
   inherit nvidia_x11;
 
-  libtorchVersion = "1.5.0";
+  libtorchVersion = "1.5.1";
   libtorchArchives = {
     x86_64-darwin-cpu = {
       url = "https://download.pytorch.org/libtorch/cpu/libtorch-macos-${libtorchVersion}.zip";
-      sha256 = "1hrhpy4yn8z5sjqwafib2wnla5isx7cg586qv1cbpdcl4am6z8v7";
+      sha256 = "1j8sjw1ldq1l2vxd9pan2jq2jn6rjr8j6jv818lz51k4jd9j3l89";
     };
     x86_64-linux-gpu = {
-      url = "https://download.pytorch.org/libtorch/cu101/libtorch-cxx11-abi-shared-with-deps-${libtorchVersion}.zip";
-      sha256 = "0yjikkxy9905li2418vnkvv8bggljjb3m22rz0llsd6mk0jlg33m";
+      url = "https://download.pytorch.org/libtorch/cu102/libtorch-cxx11-abi-shared-with-deps-${libtorchVersion}.zip";
+      sha256 = "05p9jr10jw89w37m1ylr8mx2lnhj5q37pz3d4jcympf9pi4mdq95";
     };
     x86_64-linux-cpu = {
       url = "https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-${libtorchVersion}%2Bcpu.zip";
-      sha256 = "1acz4n7d8k0cjlrysjmls05sdj9klwh0j603pdb2qdq5abclm3dl";
+      sha256 = "0sv2xiiks5iwx07ssn6sxj7hk9la32v6c0fkmrcj610d8cwz3kwd";
     };
   };
 }
