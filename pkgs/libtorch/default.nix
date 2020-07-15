2c694983794185bfbf028d13c1b83448ada41c6a
Daniël de Kok <me@danieldk.eu>
2020-07-15 08:55:00 +0200

    libtorch: use CUDA 10.1 build for GPU

--- pkgs/libtorch/default.nix
+++ pkgs/libtorch/default.nix
@@ -10,8 +10,8 @@ callPackage ./generic.nix rec {
       sha256 = "1j8sjw1ldq1l2vxd9pan2jq2jn6rjr8j6jv818lz51k4jd9j3l89";
     };
     x86_64-linux-gpu = {
-      url = "https://download.pytorch.org/libtorch/cu102/libtorch-cxx11-abi-shared-with-deps-${libtorchVersion}.zip";
-      sha256 = "05p9jr10jw89w37m1ylr8mx2lnhj5q37pz3d4jcympf9pi4mdq95";
+      url = "https://download.pytorch.org/libtorch/cu101/libtorch-cxx11-abi-shared-with-deps-${libtorchVersion}%2Bcu101.zip";
+      sha256 = "0vram4a1irgl6hspk3d8hxg6z09z41p40rpmg4r10z66vn1k4nr4";
     };
     x86_64-linux-cpu = {
       url = "https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-${libtorchVersion}%2Bcpu.zip";
